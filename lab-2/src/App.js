5769bf52572eee8fd19acc659e2f0e65530b1281
Dmitry Sedov <d-sedov@marya.ru>
2018-05-27 21:52:16 +0400

    #lab-2 add filter

--- lab-2/src/App.js
+++ lab-2/src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            searchBoxUsers: users,
             user: {},
             isUserChoosed: false,
+            search: '',
         }
     }
 
@@ -18,21 +18,49 @@ class App extends Component {
         this.setState({isUserChoosed: true});
     }
 
+    updateSearch(event) {
+        this.setState({
+          search: event.target.value
+        })
+    }
+
     render() {
         const choosedUser = this.state.isUserChoosed && <ChoosedUser data={this.state.user} />
-        console.log(this.state.user);
-        console.log(this.state.isUserChoosed);
+        const filteredUsers = users.filter(
+            (user) => {
+                let searchingByFields = '';
+                let searchStateWithoutField = this.state.search.replace(/.*:/i,'');
+                if (this.state.search.indexOf(':') !== -1) {
+                    const field = this.state.search.replace(/:.*/i,'');
+                    searchingByFields = user[field].toLowerCase();
+                } else {
+                    searchingByFields = user.name.toLowerCase()
+                                      + user.email.toLowerCase()
+                                      + user.phone.toLowerCase();
+                }
+                return searchingByFields.indexOf(
+                    searchStateWithoutField.toLowerCase()) !== -1;
+            }
+        )
       return (
         <div className="container">
             <h1>Address Book</h1>
             <div className="cards">
                 <div className="card">
                     <span className="card-summary">
-                        <input type="text" placeholder="Filter..."  />
+                        <input type="text"
+                            value={this.state.search}
+                            onChange={this.updateSearch.bind(this)}
+                            placeholder="Filter..."  
+                        />
                     </span>
+                    <div className="card-summary">
+                        Search by default for all fields.<br/>
+                        Type 'name:', 'email:' or 'phone:' to filter by this value.
+                    </div>
                     <span className="card-summary">
                         <ul>
-                            {this.state.searchBoxUsers.map(user => (
+                            {filteredUsers.map(user => (
                                 <SearchBoxUser 
                                     key={user.id} 
                                     name={user.name} 
